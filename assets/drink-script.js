aec129bdd022693345c89d908f9ca6b76990dcc7
Olivia Scalenghe <oliviascalenghe@Olivias-MacBook-Pro.local>
2023-01-12 21:45:43 -0500

    worked with food API

--- assets/drink-script.js
+++ assets/drink-script.js
@@ -3,18 +3,37 @@ var drinkIngredient = document.getElementById('drink-ingredient');
 var foodSelect = document.getElementById('food-button');
 var drinkSelect = document.getElementById('drink-button');
 
-var apiKey = 9d3356672b70422fa17b1053440d95d3
+var apiKey = "&apiKey=9d3356672b70422fa17b1053440d95d3";
 
 
 foodSelect.addEventListener('click', (event) => {
     event.preventDefault();
     let checkboxes = document.querySelectorAll('input[name="food-ingredient"]:checked');
     let output = [];
-    checkboxes.forEach((checkbox) => {
+    var foodURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+   // https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
+    checkboxes.forEach((checkbox, index) => {
+        if (index == 0) {
+            foodURL += checkbox.value;
+        } else {
+            foodURL += ",+" + checkbox.value;
+        }
         output.push(checkbox.value);
     });
+    foodURL = foodURL + apiKey;
+    console.log(foodURL);
     console.log(output);
+
+    fetch(foodURL)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    });
+    console.log(foodURL);
 });
+
+
 
 
 function getCocktailDb() {
@@ -34,3 +53,4 @@ function getCocktailDb() {
     
 };
 drinkSelect.addEventListener('click', getCocktailDb);
+
